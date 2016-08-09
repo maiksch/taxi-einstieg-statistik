551e294dd833a0b9460518c2ec2084d47399391f
Karlson Hanke <karlson.hanke@tu-clausthal.de>
2016-08-09 21:02:49 +0200

    Beginn Heatmap XML

---
+++ src/main/java/edu/tuc/taxieinstiegstatistik/service/DocumentService.java
@@ -0,0 +1,27 @@
+package edu.tuc.taxieinstiegstatistik.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Date;
+
+/**
+ * Created by Smadback on 09.08.2016.
+ */
+@Path("/getEinstiegsPunkt")
+public class DocumentService {
+
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public EinstiegsPunkt getEingstiegsPunkt() {
+
+        Document ep = new Document();
+        ep.setX("10.521111");
+        ep.setY("52.269167,0");
+        ep.setDatum(new Date());
+
+        return ep;
+    }
+
+}
