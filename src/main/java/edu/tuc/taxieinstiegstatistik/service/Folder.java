122751040c973c1b5fc84389863fa4f947d1eaf2
MST01 <mstef13@tu-clausthal.de>
2016-08-10 13:12:24 +0200

    XML Tag Struktur update

--- src/main/java/edu/tuc/taxieinstiegstatistik/service/Folder.java
+++ src/main/java/edu/tuc/taxieinstiegstatistik/service/Folder.java
@@ -6,13 +6,15 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class Folder {
 	
 	public String name;
+	public Placemark Placemark;//Großschreibung da XML case sensitive
 	
 	public Folder(){
 		
 	}
 	
-	public Folder(String name){
+	public Folder(String name,Placemark pl){
 		this.name = name;
+		this.Placemark = pl;
 	}
 	
 	//Anmerkung getter und setter werden vom Server nicht vertragen
@@ -22,9 +24,18 @@ public class Folder {
 		return name;
 	}
 	
-	 @XmlElement
+@XmlElement
 	public void setname(String name){
 		this.name = name;
 	}
+	 
+	public Placemark getplacemark(){
+		return placemark;
+	}
+@XmlElement	
+	public void setplacemark(Placemark pl){
+		this.placemark = pl;
+	}
 */
+
 }
