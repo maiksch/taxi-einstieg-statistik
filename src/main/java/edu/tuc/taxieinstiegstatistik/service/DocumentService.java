63d41c607adf2bc88e23fbd6262d4a8ed0a1cad2
MST01 <mstef13@tu-clausthal.de>
2016-08-10 11:35:58 +0200

    XML Tagstruktur weiter ausgebaut

--- src/main/java/edu/tuc/taxieinstiegstatistik/service/DocumentService.java
+++ src/main/java/edu/tuc/taxieinstiegstatistik/service/DocumentService.java
@@ -8,6 +8,34 @@ import java.util.Date;
 
 /**
  * Created by Smadback on 09.08.2016.
+ * 
+ * Benoetigte XML/KML Struktur zur Verarbeitung in OpenLayer
+ * 
+ *<?xml version="1.0" encoding="UTF-8"?>
+ *<kml xmlns="http://earth.google.com/kml/2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+ *   <Document>
+ *       <Folder>
+ *           <name>Magnitude 5</name>
+ *           <Placemark id="2012 Feb 9 18:52:48.39 UTC">
+ *               <name>2012 Feb 9 18:52:48.39 UTC</name>
+ *               <magnitude>45.0</magnitude>
+ *               <Point>
+ *                   <coordinates>10.521111,52.269167,0</coordinates>
+ *               </Point>
+ *           </Placemark>
+ *           <Placemark id="2012 Feb 9 18:52:48.39 UTC">
+ *               <name>2012 Feb 9 18:52:48.39 UTC</name>
+ *               <magnitude>5.9</magnitude>
+ *               <Point>
+ *                   <coordinates>10.521111,52.269167,0</coordinates>
+ *               </Point>
+ *           </Placemark>
+ *       </Folder>
+ *   </Document>
+ *</kml>
+ * 
+ * Die XML-Tagstruktur wird durch die Klassennamen und das Einbeziehen von weiteren Klassenobjekten aufgebaut.
+ * 
  */
 @Path("/getDocument")
 public class DocumentService {
@@ -17,11 +45,12 @@ public class DocumentService {
     public Document getEingstiegsPunkt() {
 
         Document ep = new Document();
-        Folder fl = new Folder();
-        ep.setX("10.521111");
-        ep.setY("52.269167,0");
-        ep.setDatum(new Date());
+        Folder fl = new Folder("Magnitude 5");
+ //       ep.setX("10.521111");
+ //       ep.setY("52.269167,0");
+ //       ep.setDatum(new Date());
         ep.setFolder(fl);
+ //       fl.setname("Magnitude 5");
 
         return ep;
     }
--- src/main/java/edu/tuc/taxieinstiegstatistik/service/Folder.java
+++ src/main/java/edu/tuc/taxieinstiegstatistik/service/Folder.java
@@ -1,7 +1,30 @@
 package edu.tuc.taxieinstiegstatistik.service;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+//@XmlRootElement
 public class Folder {
 	
-	public String name = "KYle";
+	public String name;
 	
+	public Folder(){
+		
+	}
+	
+	public Folder(String name){
+		this.name = name;
+	}
+	
+	//Anmerkung getter und setter werden vom Server nicht vertragen
+	//fuehrt zu "internal Server error" im Browser
+/*	
+	public String getname(){
+		return name;
+	}
+	
+	 @XmlElement
+	public void setname(String name){
+		this.name = name;
+	}
+*/
 }
