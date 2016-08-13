7254b0b3a98c076d7eb1c8086f014b66d6d3be20
MST01 <mstef13@tu-clausthal.de>
2016-08-13 16:15:38 +0200

    KML Anpassung
    Fehlende Zeilen aus übergangenem Commit

--- src/main/java/edu/tuc/taxieinstiegstatistik/service/KML.java
+++ src/main/java/edu/tuc/taxieinstiegstatistik/service/KML.java
@@ -1,25 +1,40 @@
 package edu.tuc.taxieinstiegstatistik.service;
 
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement(name = "KML")
 public class KML {
-	
-	public Document dc;
-	
-	public KML(){
-		
+
+	private Document document;
+	private String xmlns = "http://earth.google.com/kml/2.0";
+	private String xmlns_atom = "http://www.w3.org/2005/Atom";
+
+	public Document getDocument() {
+		return document;
 	}
 
-	public KML(Document dc){
-		this.dc = dc;
+	@XmlElement(name = "Document")
+	public void setDocument(Document dc) {
+		this.document = dc;
 	}
-	
-	public void setDocument(Document dc){
-		this.dc = dc;
-		
+
+	public String getXmlns() {
+		return xmlns;
 	}
-	
-	public Document getDocument(){
-	return dc;
-	}
+
+	@XmlAttribute
+    public void setXmlns(String xmlns) {
+        this.xmlns = xmlns;
+    }
+
+    public String getXmlns_atom() {
+        return xmlns_atom;
+    }
+
+    @XmlAttribute(name = "xmlns:atom")
+    public void setXmlns_atom(String xmlns_atom) {
+        this.xmlns_atom = xmlns_atom;
+    }
 }
